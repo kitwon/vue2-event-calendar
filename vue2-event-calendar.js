c5842fa4fd290f8f02a7c9add796d5e665a24ff6
kit <kit@kitwangs-MacBook-Pro.local>
2018-03-27 15:13:11 +0800

    fix import warning

--- vue2-event-calendar.js
+++ vue2-event-calendar.js
@@ -1832,8 +1832,7 @@ function loadLocale(name) {
     var oldLocale = null;
     // TODO: Find a better way to register and load all the locales in Node
     if (!locales[name] && typeof module !== 'undefined' && module && module.exports) {
-      try {
-            console.log(locales);
+        try {
             oldLocale = globalLocale._abbr;
             var aliasedRequire = require;
             aliasedRequire('./locale/' + name);
