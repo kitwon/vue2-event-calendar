2e5013d625536727f036112ca32207d7aac5e91b
kitwang <kiwang.c@outlook.com>
2019-04-25 13:16:23 +0800

    update build command & babel config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => ({
   ...(api.env('test') && { plugins: ['require-context-hook'] }),
   presets: [
     ['@vue/app', {
-      useBuiltIns: false
+      useBuiltIns: process.env.BABEL_BUILDIN ? 'usage' : false
     }]
   ]
 });
